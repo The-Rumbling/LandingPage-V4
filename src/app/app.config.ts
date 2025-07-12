09379ddd912993137c1275fa28e23b57ec5275bd
Ariana Nickole <arianalizarbe@gmail.com>
2025-07-12 10:54:28 -0500

    Actualizo HTML

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,7 +5,7 @@ import { routes } from './app.routes';
 import {HttpClient, provideHttpClient} from '@angular/common/http';
 import {provideTranslateService, TranslateLoader} from '@ngx-translate/core';
 import {TranslateHttpLoader} from '@ngx-translate/http-loader';
-const httpLoaderFactory:(http: HttpClient)=> TranslateLoader = (http: HttpClient)=> new TranslateHttpLoader(http, '/i18n/', '.json');
+const httpLoaderFactory:(http: HttpClient)=> TranslateLoader = (http: HttpClient)=> new TranslateHttpLoader(http, './assets/i18n/', '.json');
 
 export const appConfig: ApplicationConfig = {
   providers: [
